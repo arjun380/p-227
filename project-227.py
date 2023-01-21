3d5684b5fcf97f565d26373328ca2b972236d747
arjunTheCoder <82092827+arjun380@users.noreply.github.com>
2023-01-21 15:40:38 +0530

    Add files via upload

---
+++ project-227.py
@@ -0,0 +1,18 @@
+import json
+import csv
+
+data_from_csv = []
+with open('data_set.txt', 'r') as f:
+data_from_txt = json.loads(f.read())
+
+field_names = ['brake','hand_brake','throttle','steer']
+    csvfilestore = open('project_227.csv','w', newline='')
+    writer = csv.DictWriter(csvfilestore, fieldnames=field_names)
+    writer.writeheader()
+    writer.writerows(data_from_txt)
+
+with open('project_227.csv', 'r') as file:
+     reader = csv.reader(file) 
+     for row in reader :
+         data_from_csv.append(row)
+print(data_from_csv)   
